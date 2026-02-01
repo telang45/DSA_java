5f39378c070050bfee9de14e33de57b40eae7436
telang45 <keshavtelang2728@gmail.com>
2026-02-01 20:51:50 +0530

    reverse right angle triangle pattern

---
+++ pattern_program/pattern4.java
@@ -0,0 +1,23 @@
+package pattern_program;
+
+public class pattern4 {
+    public static void main(String[] args) {
+         int i,j;
+    for(i=1; i<=4; i++){
+        for(j=1; j<=5-i; j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+ }
+}
+
+
+// another logic
+//for(i=4; i>=1; i--){
+    //for(j=1; j<=i; j++){
+        //sop("*");
+    //}
+    //sopln();
+//}
+   
