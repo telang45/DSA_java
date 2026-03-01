0b6b46d57a2a7142bd75732f8bf04b4532dfcb4f
telang45 <keshavtelang2728@gmail.com>
2026-03-01 20:05:03 +0530

    floyd pyramid of number

---
+++ pattern_program/pattern8.java
@@ -0,0 +1,15 @@
+public class pattern8 {
+    public static void main(String[] args) {
+        int i,j;
+        int number=1;
+        for(i=1; i<=5; i++){
+            for(j=1; j<=i; j++){
+                System.out.print(number+" ");
+                number++;
+            }
+            
+            System.out.println();
+        }
+        
+    }
+}
