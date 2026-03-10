3011f7099ecb7d31482609d895e61d8eb3572235
telang45 <keshavtelang2728@gmail.com>
2026-03-10 19:31:52 +0530

    reverse a number

---
+++ basic_logic/Reverse_num.java
@@ -0,0 +1,17 @@
+public class Reverse_num {
+    public static void main(String[] args) {
+        int num = 5678;
+        int rev = 0;
+
+        while(num > 0){
+
+            int digit = num % 10;
+            rev = rev * 10 + digit;
+            num = num/10;
+           
+        }
+
+        System.out.println(rev);  
+
+    }
+}
