601a8e448f2061383ec1a1545d9e4a9532f9fda3
telang45 <keshavtelang2728@gmail.com>
2026-03-16 23:53:09 +0530

    finding largest number

---
+++ Arrays/array3.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class array3 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int[5];
+
+        System.out.println("Enter 5 numbers:");
+
+        // Input array elements
+        for(int i = 0; i < 5; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int largest = arr[0];
+
+        // Finding largest number
+        for(int i = 1; i < 5; i++) {
+            if(arr[i] > largest) {
+                largest = arr[i];
+            }
+        }
+
+        System.out.println("Largest number is: " + largest);
+    }
+}
