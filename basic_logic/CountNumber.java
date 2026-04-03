5e6a94f0d1002c016de0bebd03689e7de12cdffa
telang45 <keshavtelang2728@gmail.com>
2026-04-03 21:02:06 +0530

    count the numbers

---
+++ basic_logic/CountNumber.java
@@ -0,0 +1,14 @@
+public class CountNumber {
+    public static void main(String[] args) {
+
+        int num = 12345;
+        int count = 0;
+
+        while(num > 0) {
+            num = num / 10;
+            count++;
+        }
+
+        System.out.println("Digits = " + count);
+    }
+}
